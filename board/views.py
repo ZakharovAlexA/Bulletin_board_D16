a60669352ba4434cd212ecb8419e3577d7175784
Alexander Zakharov <zakharov.alex.a@mail.ru>
2023-08-05 01:12:54 +0700

    change views

--- board/views.py
+++ board/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.shortcuts import render
